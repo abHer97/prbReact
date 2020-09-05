51350d61efb89a5f2778da4a95ebb5b74fa6ebc1
Abel Herrera <abelherrera9720@gmail.com>
2020-09-05 16:47:19 -0500

    se ha creado la pagina de produccion en docs para subir a hosting, tambien se corrigieron las referencias a archivos para evitr errores

---
+++ docs/precache-manifest.e6ed3744a2ee64ef7416d8ee877a7546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62137474f4e6850448a32f65b14569fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f5de6c7df22615a0d19",
+    "url": "/static/css/main.dabea606.chunk.css"
+  },
+  {
+    "revision": "9d80d5898a0cb01d295a",
+    "url": "/static/js/2.41448e0d.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.41448e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5de6c7df22615a0d19",
+    "url": "/static/js/main.8809f901.chunk.js"
+  },
+  {
+    "revision": "def3c0411e192dd1b5f3",
+    "url": "/static/js/runtime-main.c2eaeb28.js"
+  }
+]);
